c0be9c486551aa0cb80f80b3bc0df154a59fe998
isferrei <isabelad-carvalho@hotmail.com>
2021-03-30 23:24:54 -0300

    updating grades api

--- src/routes/grades.js
+++ src/routes/grades.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import { promises as fs } from 'fs';
+
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  res.end();
+const { readFile, writeFile } = fs;
+
+router.post('/', async (req, res) => {
+  try {
+    let grade = req.body;
+
+    const data = JSON.parse(await readFile('grades.json'));
+
+    grade.id = data.nextId;
+    data.nextId++;
+    data.grades.push(grade);
+
+    await writeFile('grades.json', JSON.stringify(data));
+
+    res.send(grade);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
 });
 
 export default router;
