bcc0696c79a3c1e02c463ae13a40ad9d8f5b532f
isferrei <isabelad-carvalho@hotmail.com>
2021-03-31 07:55:38 -0300

    CRUD creation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import gradesRoute from './routes/grades.js';
 import { promises as fs, writeFile } from 'fs';
 
+global.grades = 'grades.json';
+
 const app = express();
 app.use(express.json());
 
@@ -10,14 +12,14 @@ app.use('/grades', gradesRoute);
 app.listen(3000, async () => {
   grades: [];
   try {
-    await fs.readFile('grades.json');
+    await fs.readFile(global.grades);
     console.log('API STARTED');
   } catch (err) {
     const grades = {
       nextId: 1,
       grades: [],
     };
-    writeFile('grades.json', JSON.stringify(grades))
+    writeFile(global.grades, JSON.stringify(grades))
       .then(() => {
         console.log('FILE CREATED');
       })
--- src/routes/grades.js
+++ src/routes/grades.js
@@ -5,17 +5,17 @@ const router = express.Router();
 
 const { readFile, writeFile } = fs;
 
+/* POST */
 router.post('/', async (req, res) => {
   try {
     let grade = req.body;
 
-    const data = JSON.parse(await readFile('grades.json'));
+    const data = JSON.parse(await readFile(global.grades));
 
-    grade.id = data.nextId;
-    data.nextId++;
+    grade = { id: data.nextId++, ...grade };
     data.grades.push(grade);
 
-    await writeFile('grades.json', JSON.stringify(data));
+    await writeFile(global.grades, JSON.stringify(data, null, 2));
 
     res.send(grade);
   } catch (err) {
@@ -23,4 +23,83 @@ router.post('/', async (req, res) => {
   }
 });
 
+/* GET */
+router.get('/', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.grades));
+    delete data.nextId;
+
+    res.send(data);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.grades));
+
+    const grade = data.grades.find(
+      (grade) => grade.id == req.params.id
+    ); /*  por ter '==' ele faz conversão para o parseInt automaticamente */
+
+    res.send(grade);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+/* DELETE */
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.grades));
+
+    const grade = data.grades.delete(
+      (grade) => grade.id !== parseInt(req.params.id)
+    );
+
+    await writeFile(global.grades, JSON.stringify(data, null, 2));
+
+    res.end();
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
 export default router;
+
+/* PUT */
+router.put('/', async (req, res) => {
+  try {
+    const grade = req.body;
+
+    const data = JSON.parse(await readFile(global.grades));
+    const index = data.grades.findIndex((a) => a.id === parseInt(grade.id));
+
+    data.grades[index] = grade;
+
+    await writeFile(global.grades, JSON.stringify(data, null, 2));
+
+    res.send(grade);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+/* PATCH*/
+router.patch('/', async (req, res) => {
+  try {
+    const grade = req.body;
+
+    const data = JSON.parse(await readFile(global.grades));
+    const index = data.grades.findIndex((a) => a.id === parseInt(grade.id));
+
+    data.grades[index].id = grade.id;
+
+    await writeFile(global.grades, JSON.stringify(data, null, 2));
+
+    res.send(data.grades[index]);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
